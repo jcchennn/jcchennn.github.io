a21414df91a125e8b47764b25cf677f4f3574f95
geraldine <geraldine.rj.chen@dct-cloud.com>
2026-08-10 17:27:34 +0800

    Init a vue project with daisyUI

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Hello there</h1>
+        <h3 class="py-6">I'm JC</h3>
+        <!-- <button class="btn btn-primary">Get Started</button> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
